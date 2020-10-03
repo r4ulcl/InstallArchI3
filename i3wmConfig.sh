7c92aa8c9497b5a026815ce4e74bbbd0a9700db6
Loop-Man <60947094+Loop-Man@users.noreply.github.com>
2020-10-03 16:24:34 +0200

    Update i3wmConfig.sh

--- i3wmConfig.sh
+++ i3wmConfig.sh
@@ -62,21 +62,13 @@ sudo localectl set-x11-keymap es # Set the keyboard map to es
 # sudo localectl set-x11-keymap colemak # Set the keyboard map to colemak
 
 # Config pacman
+
+if [ ! -d "/etc/pacman.d/hooks/" ]
+then
+    sudo mkdir -p /etc/pacman.d/hooks
+fi
 sudo cp mirrorupgrade.hook /etc/pacman.d/hooks/
 sudo cp pacman.conf /etc/
-
-echo "Installing go tools"
-go get -u github.com/projectdiscovery/subfinder/v2/cmd/subfinder
-go get -u github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-go get -u github.com/projectdiscovery/httpx/cmd/httpx
-go get -u github.com/tomnomnom/assetfinder
-go get -u github.com/tomnomnom/httprobe
-go get -u github.com/tomnomnom/waybackurls
-go get -u github.com/ffuf/ffuf
-go get -u github.com/lc/gau
-go get -u github.com/hakluke/hakrawler
-go get -u github.com/OJ/gobuster
-go get -u github.com/asciimoo/wuzz
 
 echo "Disabling beep sound"
 sudo -- sh -c 'echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf'

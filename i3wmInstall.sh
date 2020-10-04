94da1d266fc8735788937634fcb864aedbac0500
Loop-Man <60947094+Loop-Man@users.noreply.github.com>
2020-10-04 23:38:22 +0200

    Update i3wmInstall.sh

--- i3wmInstall.sh
+++ i3wmInstall.sh
@@ -459,6 +459,7 @@ sudo pacman -S --noconfirm --needed openbsd-netcat # Netcat que mas me gusta de 
 sudo pacman -S --noconfirm --needed lsd         # ls con esteroides
 sudo pacman -S --noconfirm --needed bat         # cat con esteroides
 sudo pacman -S --noconfirm --needed samba       # Instalar samba
+sudo pacman -S --noconfirm --needed smbmap-git  # Instala smbmap para enumerar por samba.
 
 ## Actualizo plugins, scripts y exploits
 
